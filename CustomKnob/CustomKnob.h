76d12a288cc692e98a2eb6fef9ab50928d9ac596
Husnain Ali <husnain.ali@arhamsoft.com>
2019-11-26 18:01:55 +0500

    Latest code
    Latest code

---
+++ CustomKnob/CustomKnob.h
@@ -0,0 +1,19 @@
+//
+//  CustomKnob.h
+//  CustomKnob
+//
+//  Created by MacBook on 26/11/2019.
+//  Copyright © 2019 arhamsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomKnob.
+FOUNDATION_EXPORT double CustomKnobVersionNumber;
+
+//! Project version string for CustomKnob.
+FOUNDATION_EXPORT const unsigned char CustomKnobVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomKnob/PublicHeader.h>
+
+
